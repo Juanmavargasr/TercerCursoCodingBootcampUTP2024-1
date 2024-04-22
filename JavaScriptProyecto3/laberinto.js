011c60f573346b7e16fe11eb470606f9f3ffd6fa
Juan Manuel Vargas <juanmavargasr@gmail.com>
2024-04-22 13:30:47 -0500

    maze not working

--- JavaScriptProyecto3/laberinto.js
+++ JavaScriptProyecto3/laberinto.js
@@ -42,43 +42,16 @@ const findExit = (maze) => {
 
     visitOrder.push([i, j]);
     visited[i][j] = true;
-    maze[i][j] = "©"; // Marcar la celda como visitada con el símbolo "©"
+    maze[i][j] = "©";
 
     search(i + 1, j);
     search(i - 1, j);
     search(i, j + 1);
     search(i, j - 1);
 
-    // Si no hay más caminos disponibles, retroceder
     visitOrder.pop();
     maze[i][j] = " ";
   };
-
-  // const search = (i, j) => {
-  //   if (maze[i + 1][j] === " " && visited[i][j] === false) {
-  //     visitOrder.push([i, j]);
-  //     visited[i], ([j] = true);
-  //     search(i, j);
-  //   } else if (maze[i - 1][j] === " " && visited[i][j] === false) {
-  //     visitOrder.push([i, j]);
-  //     visited[i], ([j] = true);
-  //     search(i, j);
-  //   } else if (maze[i][j + 1] === " " && visited[i][j] === false) {
-  //     visitOrder.push([i, j]);
-  //     visited[i], ([j] = true);
-  //     search(i, j);
-  //   } else if (maze[i][j - 1] === " " && visited[i][j] === false) {
-  //     visitOrder.push([i, j]);
-  //     visited[i], ([j] = true);
-  //     search(i, j);
-  //   } else {
-  //     visitOrder.pop(visitOrder.length - 1);
-  //     search(
-  //       visitOrder[visitOrder.length - 1][0],
-  //       visitOrder[visitOrder.length - 1][1]
-  //     );
-  //   }
-  // };
 
   search(0, 0);
   console.log(visitOrder);
@@ -87,53 +60,5 @@ const findExit = (maze) => {
     console.log(row.join(""));
   }
 };
-
-// const findExit2 = (maze) => {
-//   const numRows = maze.length;
-//   const numCols = maze[0].length;
-//   let visited = new Array(numRows)
-//     .fill()
-//     .map(() => new Array(numCols).fill(false));
-
-//   const dfs = (i, j) => {
-//     // Base cases: check if we are out of bounds or hitting a wall
-//     if (
-//       i < 0 ||
-//       i >= numRows ||
-//       j < 0 ||
-//       j >= numCols ||
-//       maze[i][j] === "█" ||
-//       visited[i][j]
-//     ) {
-//       return false;
-//     }
-
-//     // Check if we reached the exit
-//     if (j === numCols - 1) {
-//       maze[i][j] = "©"; // Mark the path with ©
-//       return true;
-//     }
-
-//     visited[i][j] = true;
-
-//     // Explore neighbors (up, down, left, right)
-//     if (dfs(i + 1, j) || dfs(i - 1, j) || dfs(i, j + 1) || dfs(i, j - 1)) {
-//       maze[i][j] = "©"; // Mark the path with ©
-//       return true;
-//     }
-
-//     return false;
-//   };
-
-//   // Start DFS from the entrance
-//   if (dfs(0, 0)) {
-//     // If there's a path to exit, print the maze with the path marked
-//     for (let row of maze) {
-//       console.log(row.join(""));
-//     }
-//   } else {
-//     console.log("No hay salida en el laberinto.");
-//   }
-// };
 
 findExit(createMaze());
